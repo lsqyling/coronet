e01fe494ab64de3566354040f2c2564b8c8cec32
lsqyling <1058018384@qq.com>
2026-06-29 10:01:44 +0800

    epoll fix test options

--- cmake/Option.cmake
+++ cmake/Option.cmake
@@ -4,5 +4,5 @@
 
 option(CORONET_USE_MIMALLOC "Use mimalloc as default allocator" OFF)
 option(CORONET_BUILD_TESTS "Build unit tests (gtest)" ${PROJECT_IS_TOP_LEVEL})
-option(CORONET_BUILD_BENCHMARKS "Build benchmarks (Google Benchmark)" OFF)
+option(CORONET_BUILD_BENCHMARKS "Build benchmarks (Google Benchmark)" ON)
 option(CORONET_BUILD_EXAMPLES "Build examples" ${PROJECT_IS_TOP_LEVEL})
