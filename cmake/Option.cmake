d7b28e901403b4e6954c402da1676503cedb7b6f
lsqyling <1058018384@qq.com>
2026-07-12 19:27:18 +0800

    build only coronetlib

--- cmake/Option.cmake
+++ cmake/Option.cmake
@@ -4,8 +4,8 @@
 
 option(CORONET_USE_MIMALLOC "Use mimalloc as default allocator" OFF)
 option(CORONET_IOURING "Use io_uring instead of epoll on Linux" OFF)
-option(CORONET_BUILD_TESTS "Build unit tests (gtest)" ${PROJECT_IS_TOP_LEVEL})
-option(CORONET_BUILD_BENCHMARKS "Build benchmarks (Google Benchmark)" ON)
+option(CORONET_BUILD_TESTS "Build unit tests (gtest)" OFF)
+option(CORONET_BUILD_BENCHMARKS "Build benchmarks (Google Benchmark)" OFF)
 option(CORONET_BUILD_STRESS_TESTS "Build stress/load tests (redis-benchmark or redis_loadgen)" OFF)
-option(CORONET_BUILD_EXAMPLES "Build examples" ${PROJECT_IS_TOP_LEVEL})
-option(CORONET_DEVELOPER_MODE "Build all targets for developing" ON)
+option(CORONET_BUILD_EXAMPLES "Build examples" OFF)
+option(CORONET_DEVELOPER_MODE "Build all targets for developing" OFF)
