5276f7197067858d222048c8a1eac0e9157f6ec3
lsqyling <1058018384@qq.com>
2026-07-12 19:34:06 +0800

    develop mode

--- cmake/Option.cmake
+++ cmake/Option.cmake
@@ -8,4 +8,4 @@ option(CORONET_BUILD_TESTS "Build unit tests (gtest)" OFF)
 option(CORONET_BUILD_BENCHMARKS "Build benchmarks (Google Benchmark)" OFF)
 option(CORONET_BUILD_STRESS_TESTS "Build stress/load tests (redis-benchmark or redis_loadgen)" OFF)
 option(CORONET_BUILD_EXAMPLES "Build examples" OFF)
-option(CORONET_DEVELOPER_MODE "Build all targets for developing" OFF)
+option(CORONET_DEVELOPER_MODE "Build all targets for developing" ON) # 开发分支模式，便于测试
